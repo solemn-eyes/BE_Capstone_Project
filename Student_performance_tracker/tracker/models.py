a80335e11b3269c5bb79fab816ce7a040fee9155
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-28 15:01:12 +0300

    Created a profile model for the login

--- Student_performance_tracker/tracker/models.py
+++ Student_performance_tracker/tracker/models.py
@@ -52,3 +52,16 @@ class Grade(models.Model):
         return f"{student.first_name} {student.last_name} - {self.letter_grade}"
 
 # Profile model
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('admin', 'Admin'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    department = models.CharField(max_length=50, blank=True, null=True)
+    position = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
