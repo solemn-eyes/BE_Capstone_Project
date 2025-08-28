f58299925aca5515e19fdc76d407d8c62d033a79
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-28 15:28:36 +0300

    Created a profile for the user when registering

--- Student_performance_tracker/tracker/apps.py
+++ Student_performance_tracker/tracker/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tracker'
+
+    # Importing the signals
+    def ready(self):
+        import tracker.signals
