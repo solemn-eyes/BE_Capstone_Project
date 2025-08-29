1ae9a3540e864f2b7afe3df8b26d0cd139fafbdc
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-29 21:55:11 +0300

    Added a simple homepage message

--- Student_performance_tracker/Student_tracker/urls.py
+++ Student_performance_tracker/Student_tracker/urls.py
@@ -16,14 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+def home(request):
+    return HttpResponse("Welcome to the Student Tracker API. Go to /api/ for endpoints.")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tracker.urls')),  # Include the URLs from the tracker app
+    path('', home),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
